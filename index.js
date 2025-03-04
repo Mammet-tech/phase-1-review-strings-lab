f51445053e46b131fd9c7021ad29108be96a57ac
Mammet-tech <edwinmammet19@gmail.com>
2025-03-04 10:43:35 +0300

    Complete string labs

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Mammet";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toLocaleUpperCase()}!`;
+let shortGreeting = "Welcome, Ma!"
+
